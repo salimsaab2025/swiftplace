6ef4313d273924e765aeb50a2c82bfd589ea0264
salimsaab2025 <salim@rhinoim.com>
2026-03-26 21:16:39 +0000

    Update sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'swiftplace-v1';
+const URLS_TO_CACHE = [
+  '/swiftplace/full-system.html',
+  '/swiftplace/demo-flow.html',
+  '/swiftplace/pitch.html',
+  '/swiftplace/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
